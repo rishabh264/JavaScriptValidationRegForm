87250e68012f2d2096fc339e70b340f4616f38f3
rishabh <rishbah.singhrana264@gmail.com>
2019-05-12 12:03:19 +0530

    javaScript

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/formhack.css">
+
+</head>
+<body>
+
+	<div class="container">
+		<form class="registration" id="registration">
+			<h1>Registration Form</h1>
+
+			<label for="username">
+				<span>Username</span>
+
+				<input type="text" id="username" minlength="3" required>
+
+				<ul class="input-requirements">
+					<li>At least 3 characters long</li>
+					<li>Must only contain letters and numbers (no special characters)</li>
+				</ul>
+			</label>
+            
+            
+           <label for="Email">
+                <span>Email</span>
+ 
+				<input type="text" id="Email" >
+<p id="err1"></p>
+				<ul class="input-requirements">
+
+                    <li>At least 8 characters long (and less than 100 characters)</li>
+					<li>Conatain Only name@example.com</li>
+                    
+					
+					
+				</ul>
+			</label>
+            
+<label for="Number">
+                <span>Number</span>
+
+				<input type="text" id="Number"  >
+<p id="err2"></p>
+				<ul class="input-requirements">
+
+                     <li>Phone Number should be Like  (e.g. 09585205525 or 9585205525 !)</li>
+					
+
+					
+					
+					
+				</ul>
+			</label>
+            
+			<label for="password">
+				<span>Password</span>
+
+				<input type="password" id="password" maxlength="100" minlength="8" required>
+
+				<ul class="input-requirements">
+					<li>At least 8 characters long (and less than 100 characters)</li>
+					<li>Contains at least 1 number</li>
+					<li>Contains at least 1 lowercase letter</li>
+					<li>Contains at least 1 uppercase letter</li>
+					<li>Contains a special character (e.g. @ !)</li>
+				</ul>
+			</label>
+
+			<label for="password_repeat">
+				<span>Repeat Password</span>
+				<input type="password" id="password_repeat" maxlength="100" minlength="8" required>
+			</label>
+
+			<br>
+
+			<input type="submit">
+
+		</form>
+	</div>
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,201 @@
+function CustomValidation(input) {
+	this.invalidities = [];
+	this.validityChecks = [];
+
+	//add reference to the input node
+	this.inputNode = input;
+
+	//trigger method to attach the listener
+	this.registerListener();
+}
+
+CustomValidation.prototype = {
+	addInvalidity: function(message) {
+		this.invalidities.push(message);
+	},
+	getInvalidities: function() {
+		return this.invalidities.join('. \n');
+	},
+	checkValidity: function(input) {
+		for ( var i = 0; i < this.validityChecks.length; i++ ) {
+
+			var isInvalid = this.validityChecks[i].isInvalid(input);
+			if (isInvalid) {
+				this.addInvalidity(this.validityChecks[i].invalidityMessage);
+			}
+
+			var requirementElement = this.validityChecks[i].element;
+
+			if (requirementElement) {
+				if (isInvalid) {
+					requirementElement.classList.add('invalid');
+					requirementElement.classList.remove('valid');
+				} else {
+					requirementElement.classList.remove('invalid');
+					requirementElement.classList.add('valid');
+				}
+
+			} // end if requirementElement
+		} // end for
+	},
+	checkInput: function() { // checkInput now encapsulated
+
+		this.inputNode.CustomValidation.invalidities = [];
+		this.checkValidity(this.inputNode);
+
+		if ( this.inputNode.CustomValidation.invalidities.length === 0 && this.inputNode.value !== '' ) {
+			this.inputNode.setCustomValidity('');
+		} else {
+			var message = this.inputNode.CustomValidation.getInvalidities();
+			this.inputNode.setCustomValidity(message);
+		}
+	},
+	registerListener: function() { //register the listener here
+
+		var CustomValidation = this;
+
+		this.inputNode.addEventListener('keyup', function() {
+			CustomValidation.checkInput();
+		});
+
+
+	}
+
+};
+
+var usernameValidityChecks = [
+	{
+		isInvalid: function(input) {
+			return input.value.length < 3;
+		},
+		invalidityMessage: 'This input needs to be at least 3 characters',
+		element: document.querySelector('label[for="username"] .input-requirements li:nth-child(1)')
+	},
+	{
+		isInvalid: function(input) {
+			var illegalCharacters = input.value.match(/[^a-zA-Z0-9]/g);
+			return illegalCharacters ? true : false;
+		},
+		invalidityMessage: 'Only letters and numbers are allowed',
+		element: document.querySelector('label[for="username"] .input-requirements li:nth-child(2)')
+	}
+];
+
+var EmailValidityChecks = [
+	{
+		isInvalid: function(input) {
+			return input.value.length < 8 | input.value.length > 100;
+		},
+		invalidityMessage: 'This input needs to be between 8 and 100 characters',
+		element: document.querySelector('label[for="Email"] .input-requirements li:nth-child(1)')
+	},
+	
+    {
+		isInvalid: function(input) {
+			return !input.value.match(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})/g);
+		},
+		invalidityMessage: '',
+		element: document.querySelector('label[for="Email"] .input-requirements li:nth-child(2)')
+	},
+  
+    ];
+
+
+var NumberValidityChecks = [
+{
+		isInvalid: function(input) {
+			return !input.value.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})/g);
+		},
+		invalidityMessage: '',
+		element: document.querySelector('label[for="Number"] .input-requirements li:nth-child(1)')
+	},
+];
+
+var passwordValidityChecks = [
+	{
+		isInvalid: function(input) {
+			return input.value.length < 8 | input.value.length > 100;
+		},
+		invalidityMessage: 'This input needs to be between 8 and 100 characters',
+		element: document.querySelector('label[for="password"] .input-requirements li:nth-child(1)')
+	},
+	{
+		isInvalid: function(input) {
+			return !input.value.match(/[0-9]/g);
+		},
+		invalidityMessage: 'At least 1 number is required',
+		element: document.querySelector('label[for="password"] .input-requirements li:nth-child(2)')
+	},
+	{
+		isInvalid: function(input) {
+			return !input.value.match(/[a-z]/g);
+		},
+		invalidityMessage: 'At least 1 lowercase letter is required',
+		element: document.querySelector('label[for="password"] .input-requirements li:nth-child(3)')
+	},
+	{
+		isInvalid: function(input) {
+			return !input.value.match(/[A-Z]/g);
+		},
+		invalidityMessage: 'At least 1 uppercase letter is required',
+		element: document.querySelector('label[for="password"] .input-requirements li:nth-child(4)')
+	},
+	{
+		isInvalid: function(input) {
+			return !input.value.match(/[\!\@\#\$\%\^\&\*]/g);
+		},
+		invalidityMessage: 'You need one of the required special characters',
+		element: document.querySelector('label[for="password"] .input-requirements li:nth-child(5)')
+	}
+];
+
+var passwordRepeatValidityChecks = [
+	{
+		isInvalid: function() {
+			return passwordRepeatInput.value != passwordInput.value;
+		},
+		invalidityMessage: 'This password needs to match the first one'
+	}
+];
+
+
+
+var usernameInput = document.getElementById('username');
+var EmailInput = document.getElementById('Email');
+var NumberInput = document.getElementById('Number');
+var passwordInput = document.getElementById('password');
+var passwordRepeatInput = document.getElementById('password_repeat');
+
+usernameInput.CustomValidation = new CustomValidation(usernameInput);
+usernameInput.CustomValidation.validityChecks = usernameValidityChecks;
+
+EmailInput.CustomValidation = new CustomValidation(EmailInput);
+EmailInput.CustomValidation.validityChecks = EmailValidityChecks;
+
+NumberInput.CustomValidation = new CustomValidation(NumberInput);
+NumberInput.CustomValidation.validityChecks = NumberValidityChecks;
+
+
+passwordInput.CustomValidation = new CustomValidation(passwordInput);
+passwordInput.CustomValidation.validityChecks = passwordValidityChecks;
+
+passwordRepeatInput.CustomValidation = new CustomValidation(passwordRepeatInput);
+passwordRepeatInput.CustomValidation.validityChecks = passwordRepeatValidityChecks;
+
+
+
+
+var inputs = document.querySelectorAll('input:not([type="submit"])');
+
+
+var submit = document.querySelector('input[type="submit"');
+var form = document.getElementById('registration');
+
+function validate() {
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].CustomValidation.checkInput();
+	}
+}
+
+submit.addEventListener('click', validate);
+form.addEventListener('submit', validate);
